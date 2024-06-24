bb5fa829e5ae3c34e80e24215b30bb16df47f816
goodgarry <53258052+goodgarry@users.noreply.github.com>
2024-06-24 18:25:58 +0300

    Добавьте файлы проекта.

---
+++ Geomertry/Geometry.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace Geometry
+{
+    public interface IForm
+    {
+        double GetArea();
+    }
+    public class Triangle : IForm
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double SideA, double SideB, double SideC)
+        {
+            if (SideA + SideB <= SideC || SideA + SideC <= SideB || SideB + SideC <= SideA)
+                throw new Exception("The provided sides don`t allow to construct a triangle." +
+                    "The triangle inequality rule has been violated.");
+
+            if (SideA < 0 || SideB < 0 || SideC < 0) throw new Exception("The sides must have positive values.");
+
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.SideC = SideC;
+        }
+        public double GetArea()
+        {
+            var semiperimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiperimeter * (semiperimeter - SideA) * 
+                (semiperimeter - SideB) * (semiperimeter - SideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = { SideA, SideB, SideC };
+            Array.Sort(sides);
+
+            var squareLegsSumm = Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2);
+            var squareHypothenus = Math.Pow(sides[2], 2);
+
+            return Math.Abs(squareLegsSumm - squareHypothenus) < 1e-8;
+        }
+    }
+
+    public class Circle : IForm
+    {
+        public double Radius { get; }
+
+        public Circle(double Radius)
+        {
+            if (Radius <= 0)
+                throw new ArgumentException("Radius must have positive value.");
+
+            this.Radius = Radius;
+        }
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Factory
+    {
+        public static IForm CreateForm(double sideA, double sideB, double sideC)
+        {
+
+            return new Triangle(sideA, sideB, sideC);
+        }
+
+        public static IForm CreateForm(double radius)
+        {
+            return new Circle(radius);
+        }
+    }
+
+}
---
+++ Geomertry/UnitTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+using System;
+
+namespace Geometry.Tests
+{
+    [TestClass]
+    public class ShapeTests
+    {
+        [TestMethod]
+        public void CircleAreaTest()
+        {
+            var circle = new Circle(5);
+            double area = circle.GetArea();
+            Assert.AreEqual(Math.PI * 25, area, 1e-10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CircleInvalidRadiusTest()
+        {
+            var circle = new Circle(-1);
+        }
+
+        [TestMethod]
+        public void TriangleAreaTest()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            double area = triangle.GetArea();
+            Assert.AreEqual(6, area, 1e-10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TriangleInvalidSidesTest()
+        {
+            var triangle = new Triangle(1, 2, 3);
+        }
+
+        [TestMethod]
+        public void RightTriangleTest()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            bool isRight = triangle.IsRightTriangle();
+            Assert.IsTrue(isRight);
+        }
+
+        [TestMethod]
+        public void NonRightTriangleTest()
+        {
+            var triangle = new Triangle(3, 4, 6);
+            bool isRight = triangle.IsRightTriangle();
+            Assert.IsFalse(isRight);
+        }
+
+        [TestMethod]
+        public void FormFactoryCircleTest()
+        {
+            IForm form = Factory.CreateForm(5.0);
+            Assert.IsInstanceOfType(form, typeof(Circle));
+        }
+
+        [TestMethod]
+        public void FormFactoryTriangleTest()
+        {
+            IForm shape = Factory.CreateForm(3.0, 4.0, 5.0);
+            Assert.IsInstanceOfType(shape, typeof(Triangle));
+        }
+    }
+}
